2ef6848f59832637a5f5bbd9ef46f105105082a7
Marcell Kovács <marcell.kovacs3@stud.uni-corvinus.hu>
2020-12-15 18:28:40 +0100

    commit

--- beadando_x8b4iu/beadando_x8b4iu/Form1.cs
+++ beadando_x8b4iu/beadando_x8b4iu/Form1.cs
@@ -8,13 +8,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace beadando_x8b4iu
+
+using SwimSocietyAdminApp.View;
+
+namespace SwimSocietyAdminApp
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void menuSwimmersButton_Click(object sender, EventArgs e)
+        {
+            Form swimmersForm = new SwimmersForm();
+            swimmersForm.ShowDialog();
+        }
+
+        private void menuClubsButton_Click(object sender, EventArgs e)
+        {
+            Form clubsForm = new ClubsForm();
+            clubsForm.ShowDialog();
+        }
+
+        private void menuStatsButton_Click(object sender, EventArgs e)
+        {
+            Form statsForm = new StatsForm();
+            statsForm.ShowDialog();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void swimmersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuSwimmersButton_Click(this, EventArgs.Empty);
+        }
+
+        private void clubsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuClubsButton_Click(this, EventArgs.Empty);
+        }
+
+        private void statsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuStatsButton_Click(this, EventArgs.Empty);
+        }
+
+        private void menuExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
